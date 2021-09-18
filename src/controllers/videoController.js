79ac3411eb11a2be0af55c0a2ef6ccf8c75e2b3a
Joy M <creator@Life-Is-Joy.local>
2021-09-18 22:47:45 +0900

    ADD: fake DB, route get, post

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,52 @@
+let videos = [{
+    title:"#1 First Video",
+    rating:5, 
+    comments:2,
+    createdAt:"20 minutes ago",
+    views:1,
+    id:1
+},
+{
+    title:"#2 Video",
+    rating:5, 
+    comments:2,
+    createdAt:"12 minutes ago",
+    views:45,
+    id:2
+},
+{
+    title:"#3 Video",
+    rating:5, 
+    comments:2,
+    createdAt:"1 minutes ago",
+    views:45,
+    id:3
+},
+
+]
+
 export const trending = (req,res) =>{
-    const videos = []
     res.render("home",{pageTitle:"Home",videos});
 } 
-export const see = (req,res) => res.render("watch",{pageTitle:"Watch"});
+export const watch = (req,res) => {
+    const {id}  = req.params;
+    //const id = req.params.id
+    const video = videos[id-1];
+    return res.render("watch",{pageTitle: `Watching ${video.title}`, video})
+}
+export const getEdit = (req,res) => {
+    const {id}  = req.params;
+    const video = videos[id-1];
+    return res.render("edit",{pageTitle:`Editing ${video.title}`,video});
+}
 
-export const edit = (req,res) => res.render("edit",{pageTitle:"Edit"});
-export const upload = (req,res) => res.send("Upload");
-export const deleteVideo = (req,res)=> res.send("Delete")
+export const postEdit = (req,res) =>{
+    const {id}  = req.params;
+    const {title} = req.body;
+    // videos.title =  title <---- it's not working
+    videos[id-1].title = title;
+    return res.redirect(`/videos/${id}`)
+}
 
 export const search = (req,res) => res.send("Search")
 
